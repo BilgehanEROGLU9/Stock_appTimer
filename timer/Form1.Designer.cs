88478aa49663b77cf0da16d57472279f9094e828
Bilgehan Eroğlu <bilge1999han@hotmail.com>
2021-03-03 16:50:08 +0300

    Proje dosyası ekle.

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LawnGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(158, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 117);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(245, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(513, 266);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        bool aç = true;
+        int sayac = 0;
+        public string conString = "Data Source=DESKTOP-BA5NEFA;Initial Catalog=stok1;Integrated Security=True";
+        public string conString2 = "Data Source=DESKTOP-BA5NEFA;Initial Catalog=stok2;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+            SqlConnection con = new SqlConnection(conString);
+            SqlConnection con2 = new SqlConnection(conString2);
+            SqlCommand cmd = new SqlCommand("DELETE FROM stokB", con2);
+
+            con.Open();
+            con2.Open();
+            cmd.ExecuteNonQuery();
+            cmd = new SqlCommand("SELECT * FROM stokA WHERE aktarildi='evet'", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            SqlBulkCopy bulkData = new SqlBulkCopy(con2);
+            bulkData.DestinationTableName = "stokB";
+            bulkData.WriteToServer(reader);
+            bulkData.Close();
+            con.Close();
+            con2.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (aç)
+            {
+                button1.Text = "DURDUR";
+                timer1.Start();
+                aç = false;
+            }
+            else
+            {
+                button1.Text = "BAŞLAT";
+                timer1.Stop();
+                aç = true;
+            }
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "BAŞLAT";
+            timer1.Interval = 3000;
+        }
+    }
+}
